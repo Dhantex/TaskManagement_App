69dc522c3d26f2ae54448438967008b4d8df54c9
Daniel Riveros <driveros@micredi.com.co>
2023-11-22 09:12:10 -0500

    update changes

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 import {ListGenericTask} from "../components/genericTask/ListOfTask"
+import {TaskForm} from "../components/genericTask/CreateNewTask"
 
-export function Home(){
-
-    return(
-            <React.Fragment>
-                <ListGenericTask />
-            </React.Fragment>
-        )
-}
+export function Home() {
+    const [taskListUpdated, setTaskListUpdated] = useState(false);
+  
+    // Función para actualizar la lista de tareas
+    const updateTaskList = () => {
+      setTaskListUpdated(!taskListUpdated);
+    };
+  
+    return (
+      <React.Fragment>
+        <TaskForm updateTaskList={updateTaskList} />
+        <ListGenericTask key={taskListUpdated.toString()} />
+      </React.Fragment>
+    );
+  }
--- src/components/genericTask/ListOfTask.tsx
+++ src/components/genericTask/ListOfTask.tsx
@@ -1,4 +1,4 @@
-import { Select, SelectItem } from "@tremor/react";
+// import { Select, SelectItem } from "@tremor/react";
 import {fetchDataGenericTask} from "../../Request/requestServiceApi"
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
@@ -28,11 +28,9 @@ import {
     createdBy: string;
   }
 
-
-  export function ListGenericTask() {
-
+  const ListGenericTask: React.FC = () => {
     const [data, setData] = useState<ApiResponse[]>([]);
-
+  
     useEffect(() => {
       fetchDataGenericTask()
         .then((response) => {
@@ -43,48 +41,44 @@ import {
         });
     }, []);
 
-
     return (
-      <Card style={{display: 'flex', alignItems: 'center', width: '100%'}}>
-        <Table className="table-responsive striped-table">
-          <TableHead>
-            <TableRow>
-              <TableHeaderCell>Name</TableHeaderCell>
-              <TableHeaderCell className="text-left">name</TableHeaderCell>
-              <TableHeaderCell className="text-left">description</TableHeaderCell>
-              <TableHeaderCell className="text-left">Category</TableHeaderCell>
-              <TableHeaderCell className="text-left">CreatedBy</TableHeaderCell>
-              <TableHeaderCell className="text-left">CreatedDate</TableHeaderCell>
-              <TableHeaderCell className="text-left">DueDate</TableHeaderCell>
-              <TableHeaderCell className="text-left">Status</TableHeaderCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data.map((item) => (
-              <TableRow key={item.id}>
-                <TableCell>{item.id}</TableCell>
-                <TableCell className="text-left">{item.nameGenericTask}</TableCell>
-                <TableCell className="text-left">{item.description}</TableCell>
-                <TableCell className="text-left">{item.nameCategory}</TableCell>
-                <TableCell className="text-left">{item.createdBy}</TableCell>
-                <TableCell>
-                {item.createdDate &&
-                  format(new Date(item.createdDate), 'dd/MM/yyyy HH:mm:ss')}
-                </TableCell>
-                <TableCell>
-                   {item.dueDate &&
-                   format(new Date(item.dueDate), 'dd/MM/yyyy HH:mm:ss')}
-                </TableCell>
-                <TableCell className="text-left">{item.nameStatusType}</TableCell>
-                {/* <TableCell className="text-right">
-                  <BadgeDelta deltaType={item.Status} size="xs">
-                    {item.Status}
-                  </BadgeDelta>
-                </TableCell> */}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Card>
+        <Card style={{ display: 'flex', alignItems: 'center', width: '100%' }}>
+            <Table className="table-responsive striped-table">
+                <TableHead>
+                    <TableRow>
+                        <TableHeaderCell>Name</TableHeaderCell>
+                        <TableHeaderCell className="text-left">name</TableHeaderCell>
+                        <TableHeaderCell className="text-left">description</TableHeaderCell>
+                        <TableHeaderCell className="text-left">Category</TableHeaderCell>
+                        <TableHeaderCell className="text-left">CreatedBy</TableHeaderCell>
+                        <TableHeaderCell className="text-left">CreatedDate</TableHeaderCell>
+                        <TableHeaderCell className="text-left">DueDate</TableHeaderCell>
+                        <TableHeaderCell className="text-left">Status</TableHeaderCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {data.map((item) => (
+                        <TableRow key={item.id}>
+                            <TableCell>{item.id}</TableCell>
+                            <TableCell className="text-left">{item.nameGenericTask}</TableCell>
+                            <TableCell className="text-left">{item.description}</TableCell>
+                            <TableCell className="text-left">{item.nameCategory}</TableCell>
+                            <TableCell className="text-left">{item.createdBy}</TableCell>
+                            <TableCell>
+                                {item.createdDate &&
+                                    format(new Date(item.createdDate), 'dd/MM/yyyy HH:mm:ss')}
+                            </TableCell>
+                            <TableCell>
+                                {item.dueDate &&
+                                    format(new Date(item.dueDate), 'dd/MM/yyyy HH:mm:ss')}
+                            </TableCell>
+                            <TableCell className="text-left">{item.nameStatusType}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Card>
     );
-  }
+};
+
+export {ListGenericTask};
